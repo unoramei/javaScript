68c08683c9c4f4c462066bcfccb9426d68be1c89
Jahon <shohjahonrazzakov@gmail.com>
2026-02-01 15:51:00 +0500

    Add files via upload

---
+++ intro.js
@@ -0,0 +1,8 @@
+console.log("hello world");
+console.log("hello" + " " + "world");
+console.log("hello " + "world");
+
+console.log(2 + 2)
+console.log(5 + 5);
+console.log(2 + 2 + 4);
+console.log(3 + 3 + 5);
