090ae8df1f884541b4b916b3491c6bb328c5222b
Jahon <shohjahonrazzakov@gmail.com>
2026-02-01 13:48:55 +0000

    Add files via upload

---
+++ dataTypes.js
@@ -0,0 +1,19 @@
+variable1 = "variable"
+variable2 = 5
+
+console.log(variable1);
+console.log(variable2);
+
+console.log(typeof variable1);
+console.log(typeof variable2);
+
+const message = "hello"
+console.log(typeof message);
+
+const age = 10
+console.log(typeof age);
+
+const isStudent = true
+const isFreak = false
+
+console.log(typeof isStudent, typeof isFreak);
