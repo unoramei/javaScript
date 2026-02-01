3c73e4f915dbe4e981c11ffb818b36af804682c8
Jahon <shohjahonrazzakov@gmail.com>
2026-02-01 16:14:48 +0500

    Add files via upload

---
+++ variables.js
@@ -0,0 +1,19 @@
+let variable1 = 3;
+console.log(variable1);
+
+let calculation = 2 + 2;
+console.log(calculation);
+
+console.log(calculation + 5);
+
+let result = calculation + 5
+console.log(result);
+
+let message = "Hello, World!"
+console.log(message);
+
+variable1 = 5;
+console.log(variable1);
+
+ variable1 = variable1 + 3
+console.log(variable1);
